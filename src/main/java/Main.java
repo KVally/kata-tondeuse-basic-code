0183fe1362451f9ce7c2c7eb74fec4cc2346ac2d
Vally KONATE <kvallyjunior@gmail.com>
2023-01-02 11:34:01 +0000

    Refactoring: Amélioration de la fonction principale de récupération de fichier.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import entities.Surface;
 import entities.Tondeuse;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,40 +12,60 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bonjour,\nQuel est le chemin de votre fichier d'instruction svp ?");
-        Scanner stringScanner = new Scanner(System.in);
-        String chemin = "C:\\Users\\ahmed\\Desktop\\FichierInstruction.txt" ;
-        chemin = stringScanner.nextLine();
-        try{
-            // Le fichier d'entrée
-            FileInputStream file = new FileInputStream(chemin);
-            Scanner scanner = new Scanner(file);
+        FileInputStream file = obtenirFichier();
+        Scanner scanner = new Scanner(file);
 
-            //renvoie true s'il y a une autre ligne à lire
-            int nbLigne = 1;
-            String _surface ="";
-            List<String> positionInitialeTondeuse = new ArrayList<>();
-            List<String> instructions = new ArrayList<>();
-            while(scanner.hasNextLine()) {
-                String strLigne = scanner.nextLine();
-                if (nbLigne==1) _surface = strLigne;
-                else {
-                    if (nbLigne%2==0) positionInitialeTondeuse.add(strLigne);
-                    else instructions.add(strLigne);
-                }
-                nbLigne++;
+        int nbLigne = 1;
+        String _surface ="";
+        List<String> positionInitialeTondeuse = new ArrayList<>();
+        List<String> instructions = new ArrayList<>();
+        while(scanner.hasNextLine()) {
+            String strLigne = scanner.nextLine();
+            if (nbLigne==1) _surface = strLigne;
+            else {
+                if (nbLigne%2==0) positionInitialeTondeuse.add(strLigne);
+                else instructions.add(strLigne);
             }
-            scanner.close();
-            executerLesInstructions(_surface,positionInitialeTondeuse,instructions);
-        }catch(IOException e){
-            e.printStackTrace();
+            nbLigne++;
         }
+        scanner.close();
+        executerLesInstructions(_surface,positionInitialeTondeuse,instructions);
     }
 
-    public static void executerLesInstructions(String _surface, List<String>positionInitialeTondeuses, List<String>instructions){
-        System.out.println(_surface);
-        System.out.println(positionInitialeTondeuses);
-        System.out.println(instructions);
+    public static FileInputStream obtenirFichier() {
+        System.out.println("Bonjour,\nQuel est le chemin de votre fichier d'instruction svp ?");
+        Scanner stringScanner = new Scanner(System.in);
+        String chemin = stringScanner.nextLine();
+        FileInputStream file = null;
+
+        try {
+            file = new FileInputStream(chemin);
+        } catch (FileNotFoundException e) {
+            System.out.println("Erreur ==> "+e.getMessage());
+            System.out.println("Nous allons récupérer un fichier par défaut.\n");
+            file = obtenirFichierParDefaut();
+        }
+        return file;
+    }
+
+    public static FileInputStream obtenirFichierParDefaut() {
+        try {
+            String path = String.valueOf(Main.class.getClassLoader().getResource("FichierInstructions.txt"))
+                    .replace("file:\\","").replace("file:/","");
+            FileInputStream file = new FileInputStream(path);
+            return file;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+        public static void executerLesInstructions(String _surface, List<String>positionInitialeTondeuses, List<String>instructions){
+        System.out.println("Surface => "+ _surface);
+        System.out.println("Positions initiales des tondeuses => " +positionInitialeTondeuses);
+        System.out.println("Instructuctions respectives des tondeuses => "+instructions);
         var _s= Arrays.stream(_surface.replace(" ", "").split("")).map(Integer::parseInt)
                 .collect(Collectors.toList());
         Surface surface = new Surface(_s.get(0),_s.get(1));
